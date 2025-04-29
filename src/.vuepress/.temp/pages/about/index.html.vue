13d35ef5763c48fc856dbd4b41a095373c74c73a
Misaka2298 <qzj2183221872@163.com>
2025-04-29 20:59:50 +0800

    fix: 修改github链接

--- src/.vuepress/.temp/pages/about/index.html.vue
+++ src/.vuepress/.temp/pages/about/index.html.vue
@@ -7,7 +7,7 @@
 <p>是“<strong>御坂网络</strong> 知识库”，不是“御坂 <strong>网络知识库</strong>”。</p>
 <h1 id="关于站长" tabindex="-1"><a class="header-anchor" href="#关于站长"><span>关于站长</span></a></h1>
 <p>御坂球（Misaka2298），河南郑州人，目前就读于长春市某中学高一(2024届)，现役<a href="https://www.noi.cn/" target="_blank" rel="noopener noreferrer">NOI</a>(全国青少年信息学奥林匹克竞赛)边缘选手，业余电吉他爱好者，学过一点竹笛。</p>
-<p>曾获CCF CSP-S2024复赛一等奖，参加了NOIP2024，但是没拿奖，哈哈。</p>
+<p>曾获中国民族管弦乐协会竹笛业余十级(优秀)，CCF CSP-S2024复赛一等奖，参加了NOIP2024，但是没拿奖，哈哈。</p>
 <h1 id="关于御坂网络" tabindex="-1"><a class="header-anchor" href="#关于御坂网络"><span>关于御坂网络</span></a></h1>
 <p>御坂网络是我个人搭建的各种网站的<strong>集合体</strong>，目前御坂网络有以下网站：</p>
 <p><a href="https://alist.misaka2298.icu/" target="_blank" rel="noopener noreferrer">御坂球超炫酷备份站</a></p>
