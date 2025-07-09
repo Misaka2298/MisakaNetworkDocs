8875e87f01465abe5224aa9b3ec3658f87b2a1c1
Misaka2298 <Misaka2298@outlook.com>
2025-07-09 19:55:35 +0800

    feat: 在/jl_gzzp/ 中增加了仔细阅读的提示

---
+++ src/.vuepress/.temp/pages/jl_gzzp/index.html.js
@@ -0,0 +1,16 @@
+import comp from "D:/dev/MNDocs/src/.vuepress/.temp/pages/jl_gzzp/index.html.vue"
+const data = JSON.parse("{\"path\":\"/jl_gzzp/\",\"title\":\"综合素质评价\",\"lang\":\"zh-CN\",\"frontmatter\":{\"title\":\"综合素质评价\",\"index\":false,\"icon\":\"solar:book-outline\",\"author\":\"2024届2班 乔子健\",\"date\":\"2025-07-09T00:00:00.000Z\",\"category\":[\"综评\"],\"copyright\":\"御坂网络\",\"footer\":\"使用 <a href=\\\"https://theme-hope.vuejs.press/zh/\\\" target=\\\"_blank\\\">VuePress Theme Hope</a> 主题 | MIT 协议, 版权所有 © 2025-至今 Misaka2298\",\"description\":\"每当你看见👉️符号，就说明你应该做一些事情。而其余的只供参考和更深入的理解。 如果你不想动太多脑子的话，在这个文档里面找👉️符号就可以。 电脑 > 平板 >>>>> 手机。 这个网站对手机的适配无敌无敌烂，如果你有电脑的话我强烈强烈推荐你用电脑整，没有电脑的话平板也可以，如果都没有的话手机也凑合能用，就是会写的比较难受。 如果你是在微信里打开的这个...\"},\"readingTime\":{\"minutes\":7.33,\"words\":2200},\"filePathRelative\":\"jl_gzzp/README.md\",\"localizedDate\":\"2025年7月9日\",\"autoDesc\":true}")
+export { comp, data }
+
+if (import.meta.webpackHot) {
+  import.meta.webpackHot.accept()
+  if (__VUE_HMR_RUNTIME__.updatePageData) {
+    __VUE_HMR_RUNTIME__.updatePageData(data)
+  }
+}
+
+if (import.meta.hot) {
+  import.meta.hot.accept(({ data }) => {
+    __VUE_HMR_RUNTIME__.updatePageData(data)
+  })
+}
